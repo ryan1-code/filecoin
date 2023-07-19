d673268b984504cb123762f724a8df74b3a70d29
ryan hampson <ryanhampson217@gmail.com>
2023-07-19 11:10:27 +0000

    gas challange

---
+++ contracts/gasChallenge.sol
@@ -0,0 +1,37 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.0;
+
+contract gasChallenge {
+    //Implement Fixed-Size Array Technique Here
+    uint[] numbers = [1,2,3,4,5,6,7,8,9,10];
+    
+    //Function to check for sum of array
+    //No changes required in this function
+    function getSumOfArray() public view returns (uint256) {
+        uint sum = 0;
+        for (uint i = 0; i < numbers.length; i++) {
+            sum += numbers[i];
+        }
+        return sum;
+    }
+    
+    function notOptimizedFunction() public {
+        for (uint i = 0; i < numbers.length; i++) {
+            numbers[i] = 0;
+        }
+    }
+    
+    //Implement Remaining Gas Optimization Techniques Here
+    //Sum of elements in the numbers array should equal 0
+    function optimizedFunction() public {
+      //Cache state variable
+        uint length = numbers.length;
+        //Implement unchecked block
+        unchecked {
+            //Used preincrement instead of post increment
+            for (uint i = 0; i < length; ++i) {
+                numbers[i] = 0;
+            }
+        }
+    }
+ }
